7cb2f004eced3b8bae73a9632eca14e5bf9faa8d
Ange808 <76940899+Ange808@users.noreply.github.com>
2021-01-11 14:48:39 -1000

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int f, s, t, z;
+
+     cin >> f >> s >> t >> z;
+
+     if (f > s, f > t, f > z) {
+         cout << f << endl;
+     }
+     else if (f == s, t, z){
+         cout << f << endl;
+         if (s > t, z) {
+             cout << s << endl;
+         }
+         }
+         else if (s == t, z) {
+             cout << s << endl;
+             if (t > z) {
+                 cout << t << endl;
+             }
+             }
+             else if (t = z) {
+                 cout << t << endl;
+                 if (z > f, s, z) {
+                     cout << z << endl;
+                 }
+                 }
+    return 0;
+}
